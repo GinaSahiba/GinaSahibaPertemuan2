b91194bbe391c1046acf3ef7382a50a0eb71b2b6
Gina <ginasahiba@gmail.com>
2022-03-18 09:20:13 +0700

    Percobaan Library Array

---
+++ src/main/kotlin/LibraryArray.kt
@@ -0,0 +1,9 @@
+// beberapa fungsi anggota seperti size, get(), set() yang ada di Kotlin
+
+//class Array<T> private constructor() {
+//    val size: Int
+//    operator fun get(index:Int):T
+//    operator fun set(index: Int,value:T):Unit
+//    operator fun iterator():Iterator<T>
+//    // ...
+//}
